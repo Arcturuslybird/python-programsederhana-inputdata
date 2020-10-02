4e49400aa65c31aff8a59bf056a121909b4fbb05
Arcturuslybird <72247467+Arcturuslybird@users.noreply.github.com>
2020-10-02 16:12:50 +0700

    small project01/python
    program sederhana toko

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+
+   print (      "TOKO MAINAN ANAK")
+   print("********************************")
+   nama_pembeli = str(input("Masukan nama pembeli :"))
+   kode_mainan = str(input("Masukan kode mainan :"))
+   harga = int(input("Masukan harga :"))
+   jumlah_beli = int(input("Masukan jumlah beli :"))
+   
+   print("\n ============================= \n")
+   print("Nama pembeli =",nama_pembeli)
+   print("Kode kue =",kode_mainan)
+   print("Harga  =",harga)
+   print("Jumlah beli =",jumlah_beli)
+main()
